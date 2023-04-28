37d9188d94c384501113b27d2a7d8fceb2cad176
Pedro Pacheco <pedro.jm.pacheco@outlook.pt>
2023-04-28 10:59:31 +0100

    Auto stash before merge of "feat-UI_elements" and "Develop"

---
+++ Gladimals-FP/Assets/Scripts/CameraScripts/CameraRunner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRunner : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            // rotate left
+        }
+        else if(Input.GetKey(KeyCode.Q))
+        {
+            // rotate right
+        }
+    }
+}
