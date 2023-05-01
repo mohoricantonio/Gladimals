67c22a20f8f27b2e5d0522393e4511f43b4913ca
Axel-curaudeau <axelcuraudeau@gmail.com>
2023-05-01 11:23:33 +0200

    First animations

--- Gladimals-FP/Assets/Scripts/Enemy/EnemyAI.cs
+++ Gladimals-FP/Assets/Scripts/Enemy/EnemyAI.cs
@@ -9,27 +9,31 @@ public class EnemyAI : MonoBehaviour
     [SerializeField]
     private float timeBetweenAttacks = 20f;
     public bool isAttacking = false;
-
+    public Animator animator;
 
 
     // Update is called once per frame
     void Update()
     {
         if (!isAttacking){
-            if (time >= timeBetweenAttacks){
-                time = 0f;
-                isAttacking = true;
-                GetComponent<EnemyAttack>().StartHeavyAttack();
-            }
-            else{
-                time += Time.deltaTime;
-            }
-            if (time >= timeBetweenAttacks/2 && time < timeBetweenAttacks){
-                GetComponent<EnemyMovement>().TurnArroundPlayerLeft();
-            }
-            else if (time < timeBetweenAttacks/2){
+            if (time < timeBetweenAttacks/2){
                 GetComponent<EnemyMovement>().TurnArroundPlayerRight();
+                animator.SetBool("isSideSteppingLeft", false);
+                animator.SetBool("isSideSteppingRight", true);
             }
+            else if (time >= timeBetweenAttacks/2 && time < timeBetweenAttacks){
+                GetComponent<EnemyMovement>().TurnArroundPlayerLeft();
+                animator.SetBool("isSideSteppingRight", false);
+                animator.SetBool("isSideSteppingLeft", true);
+            }
+            else if (time >= timeBetweenAttacks){
+                time = 0f;
+                animator.SetBool("isSideSteppingLeft", false);
+                animator.SetBool("isSideSteppingRight", false);
+                GetComponent<EnemyAttack>().StartHeavyAttack();
+                isAttacking = true;
+            }
+            time += Time.deltaTime;
         }
     }
 }
