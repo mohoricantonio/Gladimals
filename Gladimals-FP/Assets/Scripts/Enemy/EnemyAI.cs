9b05bd14a4c79ec519f08dd40d957f45443eaa90
Axel-curaudeau <axelcuraudeau@gmail.com>
2023-05-04 18:02:47 +0200

    Enemy Ai: stay at distance from player, and turn arround him

--- Gladimals-FP/Assets/Scripts/Enemy/EnemyAI.cs
+++ Gladimals-FP/Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,40 +1,94 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    [SerializeField]
-    private float time;
-    [SerializeField]
-    private float timeBetweenAttacks = 20f;
-    public bool isAttacking = false;
+    public float time;
+    public float timeBetweenAttacks = 20f;
     public Animator animator;
+    public EnemyMovement enemyMovement;
+    public Transform player;
 
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        if (!isAttacking){
-            if (time < timeBetweenAttacks/2){
-                GetComponent<EnemyMovement>().TurnArroundPlayerRight();
-                animator.SetBool("isSideSteppingLeft", false);
-                animator.SetBool("isSideSteppingRight", true);
+        bool isAttacking = animator.GetBool("JumpAttack");
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (!isAttacking)
+        {
+            if (distanceToPlayer > 10f)
+            {
+                Run();
             }
-            else if (time >= timeBetweenAttacks/2 && time < timeBetweenAttacks){
-                GetComponent<EnemyMovement>().TurnArroundPlayerLeft();
-                animator.SetBool("isSideSteppingRight", false);
-                animator.SetBool("isSideSteppingLeft", true);
+            else if (distanceToPlayer < 5f)
+            {
+                RunBackwards();
             }
-            else if (time >= timeBetweenAttacks){
-                time = 0f;
-                animator.SetBool("isSideSteppingLeft", false);
-                animator.SetBool("isSideSteppingRight", false);
-                GetComponent<EnemyAttack>().StartHeavyAttack();
-                isAttacking = true;
+            else if (time < timeBetweenAttacks / 2)
+            {
+                StrafeRight();
+            }
+            else if (time >= timeBetweenAttacks / 2 && time < timeBetweenAttacks)
+            {
+                StrafeLeft();
+            }
+            else if (time >= timeBetweenAttacks)
+            {
+                JumpAttack();
             }
             time += Time.deltaTime;
-        }*/
+        }
+        else
+        {
+            bool animationIsJumpAttack = animator.GetCurrentAnimatorStateInfo(0).IsName("JumpAttack");
+            float animationNormalizedTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+            if (animationIsJumpAttack && animationNormalizedTime >= 1f)
+            {
+                animator.SetBool("JumpAttack", false);
+                Debug.Log("JumpAttack ended");
+            }
+        }
+
+        enemyMovement.FocusTarget(player);
+    }
+
+    private void JumpAttack()
+    {
+        time = 0f;
+        StopMooving();
+        animator.SetBool("JumpAttack", true);
+    }
+
+    private void StrafeLeft()
+    {
+        StopMooving();
+        animator.SetBool("isSideSteppingLeft", true);
+    }
+
+    private void StrafeRight()
+    {
+        StopMooving();
+        animator.SetBool("isSideSteppingRight", true);
+    }
+
+    private void Run()
+    {
+        StopMooving();
+        animator.SetBool("isRunning", true);
+    }
+
+    private void RunBackwards()
+    {
+        StopMooving();
+        animator.SetBool("isRunningBackward", true);
+    }
+
+    private void StopMooving()
+    {
+        animator.SetBool("isRunning", false);
+        animator.SetBool("isRunningBackward", false);
+        animator.SetBool("isSideSteppingLeft", false);
+        animator.SetBool("isSideSteppingRight", false);
     }
 }
