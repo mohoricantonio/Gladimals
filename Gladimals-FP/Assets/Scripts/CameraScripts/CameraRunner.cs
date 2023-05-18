50970e57d2f62e26bfd0baff64cbef18f3982103
Pedro Pacheco <pedro.jm.pacheco@outlook.pt>
2023-05-18 16:33:33 +0100

    Cammera Runner changes

--- Gladimals-FP/Assets/Scripts/CameraScripts/CameraRunner.cs
+++ Gladimals-FP/Assets/Scripts/CameraScripts/CameraRunner.cs
@@ -7,13 +7,6 @@ public class CameraRunner : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.E))
-        {
-            transform.RotateAround(Vector3.zero, Vector3.up, -30 * Time.deltaTime);
-        }
-        else if(Input.GetKey(KeyCode.Q))
-        {
-            transform.RotateAround(Vector3.zero, Vector3.up, 30 * Time.deltaTime);
-        }
+        transform.RotateAround(Vector3.zero, Vector3.up, 15 * Time.deltaTime);
     }
 }
