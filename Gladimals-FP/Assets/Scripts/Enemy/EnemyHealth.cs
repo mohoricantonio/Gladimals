28bff299035aeb10bd204fb4471d8a2f0f463069
Axel-curaudeau <axelcuraudeau@gmail.com>
2023-06-19 15:37:10 +0200

    add enemy death

--- Gladimals-FP/Assets/Scripts/Enemy/EnemyHealth.cs
+++ Gladimals-FP/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -7,11 +7,21 @@ public class EnemyHealth : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
     public bool swordCollision = false;
+    public bool deathAnim = false;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+    }
+
+    private void Update() {
+        if(isDead() && !deathAnim){
+            deathAnim = true;
+            GetComponent<Animator>().SetTrigger("Death");
+            GetComponent<EnemyMovement>().enabled = false;
+            GetComponent<EnemyAttack>().enabled = false;
+        }
     }
 
     public void TakeDamage(int damage)
